4efc793f2db8bf722ccb09eee0cce4da9c44592b
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-12-14 19:39:45 +0300

    Complete SQL clues

--- clues.sql
+++ clues.sql
@@ -1,12 +1,11 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
 -- Write SQL query here
-SELECT name, population
-FROM countries
-WHERE region = 'Southern Europe'
-ORDER BY population ASC
+SELECT c.code, c.name, c.population
+FROM countries c
+WHERE c.region = 'Southern Europe'
+ORDER BY c.population ASC
 LIMIT 1;
-
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
@@ -14,48 +13,50 @@ LIMIT 1;
 -- Write SQL query here
 SELECT cl.language
 FROM countrylanguages cl
-JOIN countries c ON cl.countrycode = c.code
-WHERE c.name = 'Holy See (Vatican City State)'
-AND cl.isofficial = 'T';
-
+WHERE cl.countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-SELECT c.name
+SELECT c.code, c.name
 FROM countries c
 JOIN countrylanguages cl ON c.code = cl.countrycode
 WHERE cl.language = 'Italian'
-GROUP BY c.name
-HAVING COUNT(*) = 1;
+  AND c.code <> 'VAT'
+  AND c.code IN (
+    SELECT countrycode
+    FROM countrylanguages
+    GROUP BY countrycode
+    HAVING COUNT(language) = 1
+  );
+
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 SELECT ci.name
 FROM cities ci
-JOIN countries co ON ci.countrycode = co.code
-WHERE co.name = 'Holy See (Vatican City State)'
-AND ci.name != co.name;
-
+WHERE ci.countrycode = 'SMR'
+  AND ci.name <> 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-SELECT ci.name, co.name AS country
+SELECT ci.name AS city, co.name AS country
 FROM cities ci
 JOIN countries co ON ci.countrycode = co.code
-WHERE ci.name LIKE '%City%'
-AND co.continent = 'South America';
-
+WHERE ci.name LIKE 'Serr%'
+  AND co.continent = 'South America';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
-SELECT 'No capital found' AS result;
-
+SELECT ci.name AS capital
+FROM countries co
+JOIN cities ci ON co.capital = ci.id
+WHERE co.name = 'Brazil';
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -69,6 +70,6 @@ SELECT 'No capital found' AS result;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-SELECT name, population
-FROM cities
-WHERE population = 91084;
+SELECT ci.name, ci.countrycode, ci.population
+FROM cities ci
+WHERE ci.population = 91084;
